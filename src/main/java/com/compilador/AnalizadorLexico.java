2474d5f389f17d5c11685a252fb8788feae4973f
Guillermo G <6uillermo1@gmail.com>
2026-04-09 17:56:19 -0300

    Mejora de legibilidad

--- src/main/java/com/compilador/AnalizadorLexico.java
+++ src/main/java/com/compilador/AnalizadorLexico.java
@@ -6,11 +6,27 @@ import org.antlr.v4.runtime.Token;
 
 public class AnalizadorLexico {
     public static void main(String[] args) throws Exception {
-        CharStream input = CharStreams.fromString("int x = 10;");
+        boolean soportaColor = System.console() != null && System.getenv("TERM") != null;
+        String RED = soportaColor ? "\u001B[31m" : "";
+        String RESET = soportaColor ? "\u001B[0m" : "";
+
+        CharStream input;
+        if (args.length > 0) {
+            input = CharStreams.fromFileName(args[0]);
+        } else {
+            System.out.println("Ingrese el código fuente (Ctrl+Z para finalizar):");
+            input = CharStreams.fromStream(System.in);
+        }
         MiLenguajeLexer lexer = new MiLenguajeLexer(input);
         Token token;
+        System.out.println("\nTokens reconocidos:");
         while ((token = lexer.nextToken()).getType() != Token.EOF) {
-            System.out.println(token.getText() + " -> " + token.getType());
+            String nombre = MiLenguajeLexer.VOCABULARY.getSymbolicName(token.getType());
+            if (nombre == null) {
+                System.err.println(RED + "[Error léxico] Linea " + token.getLine() + ", Columna " + token.getCharPositionInLine() + ": '" + token.getText() + "'" + RESET);
+            } else {
+                System.out.printf("%-12s | %-10s | Linea:%d Col:%d\n", nombre, token.getText(), token.getLine(), token.getCharPositionInLine());
+            }
         }
     }
 }
