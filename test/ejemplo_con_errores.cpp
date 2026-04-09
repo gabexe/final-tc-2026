fb67ce0fe4be357fba4396112043a6718ed2571f
Guillermo G <6uillermo1@gmail.com>
2026-04-09 16:05:12 -0300

    Ejemplos codigo C++

---
+++ test/ejemplo_con_errores.cpp
@@ -0,0 +1,10 @@
+// Programa de prueba con errores léxicos y sintácticos
+int main() {
+    int x = 10
+    double y = 2.5.3;
+    char c = 'abc';
+    if (x > ) {
+        x = x + ;
+    }
+    return 0
+}
---
+++ test/ejemplo_correcto.cpp
@@ -0,0 +1,10 @@
+// Programa de prueba correcto para el compilador
+int main() {
+    int x = 10;
+    double y = 2.5;
+    char c = 'a';
+    if (x > 0) {
+        x = x + 1;
+    }
+    return 0;
+}
