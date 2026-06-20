ea8f45a45f100bea366561f18bd22647fbd9a2f5
Guillermo G <6uillermo1@gmail.com>
2026-06-20 16:27:00 -0300

    prueba: Añadir caso de prueba de detección de variables inútiles
    Se crea el archivo en C++ 'ejemplo_variables_innecesarias.cpp' que contiene múltiples declaraciones de variables redundantes. Esto permite comprobar la eficacia y correcto funcionamiento del 'DetectorVariablesInnecesarias' dentro del ciclo de desarrollo.

---
+++ test/ejemplo_variables_innecesarias.cpp
@@ -0,0 +1,13 @@
+int main() {
+    int x = 10;
+    int y = 20;
+    int z = x + y;
+    
+    int inutil1 = 100;
+    int inutil2 = 200;
+    
+    int w = 50;
+    int resultado = w * 2;
+    
+    return resultado;
+}
